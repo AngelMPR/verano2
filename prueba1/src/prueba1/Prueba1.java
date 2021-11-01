d7ce03646483a2661f08d91a305fd3b9d8400f79
PC <PC@DESKTOP-OJGDHRR.mshome.net>
2021-10-31 20:25:36 -0500

    v1

---
+++ prueba1/src/prueba1/Prueba1.java
@@ -0,0 +1,15 @@
+
+package prueba1;
+
+/**
+ *
+ * @author PC
+ */
+public class Prueba1 {
+
+  
+    public static void main(String[] args) {
+        System.out.println("hola como estas:");
+    }
+    
+}
